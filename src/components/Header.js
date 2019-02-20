a0b81b98fb28fe2901997e0ccba4f377dcc8859a
= <quoc_pham@msn.com>
2019-02-19 20:44:33 -0600

    Initial setup of App and Header.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>this is the header.</p>
+      </div>
+    );
+  }
+}
